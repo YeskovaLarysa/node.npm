6ac1af70f921bdc3d2f685b3194c81b6cea7b99f
Larysa Yeskova <yeskova.larysa@gmail.com>
2021-03-20 15:57:07 +0200

    add tests function dowbleElem
    добавлено тестирование функции

--- js_test/js/funcions.js
+++ js_test/js/funcions.js
@@ -1,2 +1,16 @@
 
-const dowbleElem = (num) => num*2
+// const dowbleElem = (arr) => arr.map(function (elem) {
+//   return elem*2
+// })
+
+// arr.every(elem => typeof elem === "number") // если каждый элемент масива число, то вернет true
+
+const dowbleElem = (arr) => {
+  if(Array.isArray(arr)) {
+    return arr.map(function (elem) {
+      if(typeof elem === "number"){
+        return elem*2} else {return `error: ${elem}`}
+      })
+    }
+    else {return 'error'}
+  }
--- js_test/js/test.js
+++ js_test/js/test.js
@@ -1,1 +1,11 @@
+describe('dowbleElem', () => {
+  it('введенный елемент является массивом', () => {
+    assert.equal(Array.isArray(dowbleElem(array)), true)
+  })
+})
 
+describe('dowbleElem', () => {
+  it('Не все елементы массива являются числом', () => {
+    assert.equal(dowbleElem(array.every(elem => typeof elem === "number")), true)
+  })
+})
