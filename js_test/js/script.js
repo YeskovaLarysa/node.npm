858728ac00997131665641286b7240cd99580034
YeskovaLarysa <80044571+YeskovaLarysa@users.noreply.github.com>
2021-03-21 14:49:26 +0200

    Update script.js

--- js_test/js/script.js
+++ js_test/js/script.js
@@ -1,4 +1,4 @@
 // const array = [2, 'gddndgthd', 6, 8]
-const array = '[2, 4, 6, 8]'
-// const array = [2, 4, 6, 8]
+// const array = '[2, 4, 6, 8]'
+const array = [2, 4, 6, 8]
 console.log(dowbleElem(array));
